d32637ff3ab8901260d8577280d7c0bd430e2336
pratham7512 <115919176+pratham7512@users.noreply.github.com>
2023-05-27 01:43:54 +0530

    Update MedEzy.py

--- MedEzy.py
+++ MedEzy.py
@@ -5,18 +5,9 @@ from llama_index import LLMPredictor, ServiceContext
 import sys
 #from google.colab import drive
 import os
+import pyttsx3
 import streamlit as st
-
-# Set the page title
-st.set_page_config(page_title="MedEzy")
-
-st.sidebar.title('MedEzy')
-if st.sidebar.button("New Chat"):
-    clear_generated()
-
-API_KEY=str(st.sidebar.text_input("Your OpenAI API key :",placeholder="Enter to submit"))
-os.environ["OPENAI_API_KEY"] = API_KEY
-
+txt_run=True
 def construct_index(directory_path):
   # set maximum input size
   max_input_size = 4096
@@ -47,9 +38,29 @@ def ask_bot(query):
   response = index.query(query)
   return response
 
-def clear_generated():
-    st.session_state["generated"] = []
-    st.session_state['past'] = []
+def text2speech(text):
+    txt_run=False
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 160)
+    # Convert text to speech
+    engine.say(text)
+    engine.runAndWait()
+    txt_run=True
+
+def clear_generated(generated_list,past_list):
+    generated_list.clear()
+    past_list.clear()
+    
+
+# Set the page title
+st.set_page_config(page_title="MedEzy")
+
+st.sidebar.title('MedEzy')
+if st.sidebar.button("New Chat"):
+    clear_generated(st.session_state['generated'],st.session_state['past'])
+
+API_KEY=str(st.sidebar.text_input("Your OpenAI API key :",placeholder="Enter to submit"))
+os.environ["OPENAI_API_KEY"] = API_KEY
 
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
@@ -62,31 +73,38 @@ st.title("MedEzy")
 query= st.text_input("🤖Ask AI", placeholder="Enter to submit")
 
 output=" "
-if API_KEY:
+if API_KEY and txt_run:
     st.sidebar.success('API key entered successfully.')
-    st.sidebar.info(" Medical knowledge is constantly evolving, so please keep in mind that my responses are based on the information available up until September 2021.")
-    index = construct_index("./data")
+    st.sidebar.write(" Medical knowledge is constantly evolving, so please keep in mind that my responses are based on the information available up until September 2021.")
+    index = construct_index("D:\DSA\codehelp\contents")
     if query:
         output = ask_bot(query)
         # store the output
         st.session_state.past.append(query)
         st.session_state.generated.append(str(output))
-    
+    else:
+        if len(st.session_state.generated)==0:
+            st.success("""👋 Hi there,
 
+Ask me anything related to medical queries, and I'll do my best to assist you! """,icon="🤖")
+            text2speech("Hi there, Ask me anything related to medical queries, and I'll do my best to assist you!")
     with st.expander("Conversation", expanded=True):
         for i in range(len(st.session_state['generated'])-1, -1, -1):
             st.info(st.session_state["past"][i],icon="🧐")
             st.success(st.session_state["generated"][i], icon="🤖")
 
-    #text2speech(str(output))
+        text2speech(str(output))
 else:
-    st.sidebar.error("Enter your OpenAI API key!!")
-    st.sidebar.info(" Medical knowledge is constantly evolving, so please keep in mind that my responses are based on the information available up until September 2021.")    
-    st.success("""👋 Welcome to the MedEzy Bot! 🩺
+    if txt_run:
+        st.sidebar.error("Enter your OpenAI API key!!")
+        if st.sidebar.button("Get your OpenAI API key"):
+            st.experimental_set_query_params(link="https://platform.openai.com/account/api-keys")
+        st.success("""👋 Welcome to the MedEzy Bot! 🩺
 
 I'm here to provide you with accurate and reliable information on various medical topics. Whether you have questions about symptoms, treatments, medications, preventive care, or nutrition, I'm here to help. Please remember to consult a healthcare professional for personalized advice.
 
 Ask me anything related to medical queries, and I'll do my best to assist you! """,icon="🤖")
+        st.success("Open sidebar from left and enter your OpenAI API key to chat with Me")
+        text2speech("Open sidebar from left and enter your Open A.I. A.P.I. key to chat With Me")
     
-
 
